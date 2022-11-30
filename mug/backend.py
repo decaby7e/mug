1a8d26f4538737280e80ba3118363c909f7b4bdb
decaby7e <decaby7e@ranvier.net>
2022-11-30 11:53:03 -0500

    feat: logic to invoke original backend

--- mug/backend.py
+++ mug/backend.py
@@ -5,17 +5,19 @@ Implemented according to https://www.cups.org/doc/api-filter.html
 """
 
 import os
+import signal
 import sys
+import pwd
+import time
 
 from pdfrw import PdfReader
-from typing import List
+from typing import BinaryIO, List, Optional
 from mug import connections, utils, models
+from mug.models import Account
 
 
-class FilterError(Exception):
-    def __init__(self, msg):
-        self.msg = msg
-        super(msg)
+class BackendError(Exception):
+    """Exceptions encountered during backend processing"""
 
 
 class CUPSFilter:
@@ -26,7 +28,9 @@ class CUPSFilter:
     output. The backend is the last filter in the chain and writes to the device. [1]
     """
 
-    def __init__(self, argv: List[str], cups_env: dict, print_data: bytes = None):
+    def __init__(
+        self, argv: List[str], cups_env: dict, print_data: Optional[BinaryIO] = None
+    ):
 
         # Mug initalization
 
@@ -64,7 +68,7 @@ class CUPSFilter:
 
     def dispatch(self):
         if self.env["CONTENT_TYPE"] != "application/pdf":
-            raise FilterError(
+            raise BackendError(
                 "Filetype was not 'application/pdf'! (currently only PDF files can be accounted for)"
             )
 
@@ -99,7 +103,11 @@ class CUPSFilter:
     """
 
     def count_pages(self) -> int:
-        pdf = PdfReader(fdata=self.print_data)
+        if self.print_data is None:
+            raise BackendError(
+                "Cannot count number of pages because print data does not exist!"
+            )
+        pdf = PdfReader(fdata=self.get_print_data())
         self.page_count = len(pdf.pages)
         return self.page_count
 
@@ -111,25 +119,112 @@ class CUPSFilter:
         """
 
         account_obj = (
-            self.db_session.query(models.Account).filter(username=self.job_user).first()
+            self.db_session.query(models.Account)
+            .filter(Account.username == self.job_user)
+            .first()
         )
 
         if not account_obj:
             self.return_no_user()
+
+        # HACK: account_obj.pages_printed has been changed to default 0
+        #       but might still be null in old implememtations
+        if account_obj.pages_printed is None:
+            account_obj.pages_printed = 0
 
         return account_obj.quota - account_obj.pages_printed
 
     def send_to_backend(self):
         """
         Emulate the same procedures CUPS uses to invoke backends for printing
+        Derived from Pykota's CUPSBackend.runOriginalBackend
         """
-        pass
+
+        # DEBUG
+        def write_debug_env(msg):
+            print(msg)
+            print(f"{os.geteuid()=}")
+            print(f"{originalbackend=}")
+            print(f"{arguments=}")
+            print(f"{os.environ=}")
+
+        # socket://192.168.1.1:6000
+        # mug://socket://192.168.1.1:6000
+        # mug://cups-pdf:/
+        # TODO: This needs to be done rigorously (proper URI parsing)
+        backend_bin = os.environ["DEVICE_URI"].split(":")[1][2:]
+        original_uri = os.environ["DEVICE_URI"][6:]  # TODO: Assuming prefix is mug://
+        os.environ["DEVICE_URI"] = original_uri
+
+        originalbackend = os.path.join(os.path.split(sys.argv[0])[0], backend_bin)
+        if not os.environ["CUPS_FILETYPE"] == "job-sheet":
+            arguments = [os.environ["DEVICE_URI"]] + sys.argv[1:]
+        else:
+            # For banners, we absolutely WANT
+            # to remove any filename from the command line!
+            arguments = [os.environ["DEVICE_URI"]] + sys.argv[1:6]
+        arguments[2] = self.job_user  # in case it was overwritten by external script
+        # TODO : do something about job-billing option, in case it was overwritten as well...
+        # TODO : do something about the job title : if we are printing a banner and the backend
+        # TODO : uses the job's title to name an output file (cups-pdf:// for example), we're stuck !
+    
+        self.become_root()
+
+        pid = os.fork()
+
+        # In the child of the fork
+        if pid == 0:
+            if self.print_data is not None:
+                # Redirecting file handle to real backend's stdin
+                os.dup2(self.print_data.fileno(), 0)
+
+            os.execve(originalbackend, arguments, os.environ)
+
+            # We shouldn't be here!
+            os._exit(-1)
+
+        # In the parent of the fork
+
+        self.drop_root()
+
+        # Waiting for original backend to exit...
+        killed = 0
+        status = -1
+        while status == -1:
+            try:
+                status = os.waitpid(pid, 0)[1]
+                time.sleep(1)
+            except OSError as e:
+                # TODO: Once we have SIGTERM handling:
+                #       `and self.gotSigTerm``
+                if e.errno == 4:
+                    os.kill(pid, signal.SIGTERM)
+                    killed = 1
+
+        if os.WIFEXITED(status):
+            status = os.WEXITSTATUS(status)
+            message = f"CUPS backend {originalbackend} returned {status}."
+            self.print_info(message)
+            if status:
+                level = "error"
+                self.Reason = message
+            else:
+                level = "info"
+            exit(status)
+        elif not killed:
+            self.Reason = f"CUPS backend {originalbackend} died abnormally."
+            self.print_info(self.Reason)
+            exit(-1)
+        else:
+            self.Reason = f"CUPS backend {originalbackend} was killed."
+            self.print_info(self.Reason)
+            exit(1)
 
     """
     Environment initialization and preparation
     """
 
-    def get_print_data_from_env(self) -> bytes:
+    def get_print_data_from_env(self) -> BinaryIO:
         """
         Determines whether to get job data from stdin or from the CUPS data directory
         See https://www.cups.org/doc/api-filter.html#OVERVIEW for more info
@@ -140,20 +235,24 @@ class CUPSFilter:
         fs_file = None
         if len(sys.argv) == 7:
             fs_file = sys.argv[6]
+
         stdin_buffer = sys.stdin.buffer
 
         if fs_file:
-            with open(fs_file, "rb") as f:
-                data = f.read()
+            return open(fs_file, "rb")
         elif stdin_buffer:
-            with stdin_buffer as stdin:
-                while buffer := stdin.read():
-                    data += buffer
+            return stdin_buffer
+
         else:
-            raise FilterError(
+            raise BackendError(
                 "Could not find print data from standard input or ARGV[6]"
             )
 
+    def get_print_data(self) -> bytes:
+        if self.print_data is None:
+            raise BackendError("Cannot read print data because none exists!")
+        data = self.print_data.read()
+        self.print_data.seek(0, 0)
         return data
 
     def get_job_info_from_env(self) -> dict:
@@ -182,11 +281,27 @@ class CUPSFilter:
 
         return job
 
+    @staticmethod
+    def become_root():
+        os.seteuid(0)
+        os.setegid(0)
+
+    @staticmethod
+    def drop_root():
+        uid = os.geteuid()
+        original_user = pwd.getpwuid(uid)
+
+        os.seteuid(original_user.pw_uid)
+        os.setegid(original_user.pw_gid)
+
     """
-    Exit functions
+    Message functions
     See https://www.cups.org/doc/api-filter.html#MESSAGES for more info
     TODO: Ensure messages have the most useful, least verbose information in them (e.g. usernames)
     """
+
+    def print_info(self, msg):
+        utils.stderr(f"INFO: {msg}")
 
     def return_no_content(self):
         utils.stderr("ERROR: job data has no content that can be printed")
